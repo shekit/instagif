1606326f693c6ac402c9760856d6c95b347aa87b
shekit <as7790@nyu.edu>
2017-08-13 22:13:50 -0400

    test script to fade in video

---
+++ node-camera/omx-test.js
@@ -0,0 +1,44 @@
+var spawn = require('child_process').spawn
+var path = require('path')
+
+var gifLocation = path.join(process.cwd(), 'gif.mp4')
+//console.log(omx.pid)
+
+var alphas = [0,10,50,100,150,200,255]
+
+
+function start(){
+
+	var img = spawn('sudo',['fbi','-T','2','-noverbose','black.png'])
+
+	setTimeout(function(){
+		for(var i=0;i<alphas.length;i++){
+			spawnNew(alphas[i],i)
+		}
+	}, 2000)
+
+	setTimeout(function(){
+		var kill = spawn('kill',[img.pid])
+	},12000)
+	
+}
+
+function spawnNew(alphaVal, time){
+	var omx = null
+
+	// no idea why the pid is 8 more than what node says it is
+	setTimeout(function(){
+		omx = spawn('omxplayer', ['--loop','--layer',time,'--no-osd','--alpha',alphaVal,gifLocation])
+		console.log("spawn video", omx.pid+8)
+			
+	},time*5000)
+
+	setTimeout(function(){
+		console.log("kill video", omx.pid+8);
+		var kill = spawn('kill',[omx.pid+8]);
+	}, time*5000+7000)
+
+}
+
+start()
+
